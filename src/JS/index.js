1b5e75e800551fbca29c1b4f8358d676ca1b5222
Luisa Fer <l.f.d.sando@gmail.com>
2023-01-07 17:00:22 -0500

    Cargo ejercicios de repaso

---
+++ src/JS/index.js
@@ -0,0 +1,83 @@
+'use strict';
+const $form=document.getElementById('form');
+const $legend=document.getElementsByTagName("legend")[0];
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+
+
+const formulario={ //Objeto, se declara llave valor
+   user:null,
+   pass:null
+};
+
+
+const alert = (message, type) => {
+   const wrapper = document.createElement('div');
+   wrapper.innerHTML = [
+     `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+     `   <div>${message}</div>`,
+     '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+     '</div>',
+   ].join('');
+   alertPlaceholder.append(wrapper);
+ };
+
+$form.user.addEventListener('input', (e) =>{
+ formulario.user=e.target.value;
+});
+
+$form.pass.addEventListener('input', (e) =>{
+   formulario.pass=e.target.value;
+});
+
+$form.addEventListener('submit',(e)=>{
+   e.preventDefault();
+
+   if (verFormulario () === true) {
+      verUser(formulario);
+    } else {
+      alert('*Todos los campos son obligatorios.', 'danger');
+    }
+
+   /* if(!formulario.user){
+      $legend.classList.add('text-danger');
+      console.log('error...');
+   } */
+
+});
+
+function verFormulario() {
+   if (formulario.user && formulario.pass)
+     return true;
+   else return false;
+ }
+
+ function verUser(obj){
+  /*  const permitido={
+      user:"Luisa",
+      pass:1030,
+   };
+   if(obj.user===permitido.user && 
+      parseInt(obj.pass)===permitido.pass){
+      console.log("Bienvenido");
+   }else{
+      console.log("No permitido");
+   } */
+ try{
+    const permitido={
+      user:"Luisa",
+      pass:1030,
+   };
+   if(obj.user===permitido.user && 
+      parseInt(obj.pass)===permitido.pass){
+      
+       window.open("http://127.0.0.1:5500/exercises.html","_self"); 
+   }else{
+      alert('¡Acceso denegado! revise sus credenciales', 'warning');
+   } 
+ }catch(error){
+   console.log('Se produjo un error, en la función verificación de usuario', error);
+ }
+}
+
+
+
